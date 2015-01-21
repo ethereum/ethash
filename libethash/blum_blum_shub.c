b24538419c45c24e777c55f913745894e4350c5c
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2015-01-21 12:50:59 -0800

    Inlining stuff

--- libethash/blum_blum_shub.c
+++ libethash/blum_blum_shub.c
@@ -1,44 +1,6 @@
 #include <stdint.h>
 #include "blum_blum_shub.h"
 
-uint32_t cube_mod_safe_prime1(const uint32_t x) {
-    uint64_t temp = x;
-    temp *= x;
-    temp %= SAFE_PRIME;
-    temp *= x;
-    return (uint32_t) (temp % SAFE_PRIME);
-}
-
-uint32_t cube_mod_safe_prime2(const uint32_t x) {
-    uint64_t temp = x;
-    temp *= x;
-    temp %= SAFE_PRIME2;
-    temp *= x;
-    return (uint32_t) (temp % SAFE_PRIME2);
-}
-
-
-static const uint32_t powers_of_three_mod_totient1[32] = {
-        3, 9, 81, 6561, 43046721, 3884235087U, 4077029855U, 106110483U,
-        2292893763U, 2673619771U, 2265535291U, 2641962139U, 867632699U,
-        4234161123U, 4065670495U, 1161610561U, 1960994291U, 683176121U,
-        1539788995U, 1214448689U, 2554812497U, 2574646649U, 3290602031U,
-        2381552417U, 3391635653U, 639421717U, 1685119297U, 4206074945U,
-        1006214457U, 102532655U, 4081098815U, 3106101787U
-};
-
-uint32_t three_pow_mod_totient1(uint32_t p) {
-    uint64_t r = 1;
-    for(int i = 0; i < 32; ++i) {
-        if (p & 1) {
-            r *= powers_of_three_mod_totient1[i];
-            r %= SAFE_PRIME_TOTIENT;
-        }
-        p >>= 1;
-    }
-    return (uint32_t) r;
-}
-
 void init_power_table_mod_prime1(uint32_t table[32], const uint32_t n) {
     uint64_t r = n;
     r %= SAFE_PRIME;
--- libethash/blum_blum_shub.h
+++ libethash/blum_blum_shub.h
@@ -14,7 +14,8 @@
   You should have received a copy of the GNU General Public License
   along with cpp-ethereum.  If not, see <http://www.gnu.org/licenses/>.
 */
-/** @file ethash.h
+
+/** @file blum_blum_shub.h
  * @author Matthew Wampler-Doty <tim@ethdev.org>
  * @date 2015
  */
@@ -30,11 +31,42 @@ extern "C" {
 #define SAFE_PRIME_TOTIENT_TOTIENT 2147483542
 #define SAFE_PRIME2 4294965887U
 
-// maybe want to inline some of these?
+static inline uint32_t cube_mod_safe_prime1(const uint32_t x) {
+    uint64_t temp = x;
+    temp *= x;
+    temp %= SAFE_PRIME;
+    temp *= x;
+    return (uint32_t) (temp % SAFE_PRIME);
+}
 
-uint32_t cube_mod_safe_prime1(const uint32_t x);
-uint32_t cube_mod_safe_prime2(const uint32_t x);
-uint32_t three_pow_mod_totient1(uint32_t p);
+static inline uint32_t cube_mod_safe_prime2(const uint32_t x) {
+    uint64_t temp = x;
+    temp *= x;
+    temp %= SAFE_PRIME2;
+    temp *= x;
+    return (uint32_t) (temp % SAFE_PRIME2);
+}
+
+static inline uint32_t three_pow_mod_totient1(uint32_t p) {
+    static const uint32_t powers_of_three_mod_totient1[32] = {
+            3, 9, 81, 6561, 43046721, 3884235087U, 4077029855U, 106110483U,
+            2292893763U, 2673619771U, 2265535291U, 2641962139U, 867632699U,
+            4234161123U, 4065670495U, 1161610561U, 1960994291U, 683176121U,
+            1539788995U, 1214448689U, 2554812497U, 2574646649U, 3290602031U,
+            2381552417U, 3391635653U, 639421717U, 1685119297U, 4206074945U,
+            1006214457U, 102532655U, 4081098815U, 3106101787U
+    };
+    uint64_t r = 1;
+    for(int i = 0; i < 32; ++i) {
+        if (p & 1) {
+            r *= powers_of_three_mod_totient1[i];
+            r %= SAFE_PRIME_TOTIENT;
+        }
+        p >>= 1;
+    }
+    return (uint32_t) r;
+}
+
 void init_power_table_mod_prime1(uint32_t table[32], const uint32_t n);
 uint32_t quick_bbs(const uint32_t power_table[32], const uint64_t p);
 
