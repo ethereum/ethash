556c88053a3c1594aab2066b9a6d626d5e06df5f
Tim Hughes <tim@twistedfury.com>
2015-02-17 00:24:23 +0000

    New implementation of OpenCL miner. - Doubles performance. - Implemented search function useful for mining.

--- libethash-cl/ethash_cl_miner.h
+++ libethash-cl/ethash_cl_miner.h
@@ -6,28 +6,38 @@
 #include <time.h>
 #include "../libethash/ethash.h"
 
-class ethash_miner
+class ethash_cl_miner
 {
-};
+public:
+	struct search_hook
+	{
+		// reports progress, return true to abort
+		virtual bool found(uint64_t const* nonces, uint32_t count) = 0;
+		virtual bool searched(uint64_t start_nonce, uint32_t count) = 0;
+	};
 
-class ethash_cl_miner : public ethash_miner
-{
 public:
 	ethash_cl_miner();
 
-	bool init(ethash_params const& params, const uint8_t seed[32]);
+	bool init(ethash_params const& params, const uint8_t seed[32], unsigned workgroup_size = 64);
 
-	clock_t hashes(uint8_t* ret, uint8_t const* header, uint64_t nonce, unsigned count, unsigned HASH_SIZE = 0, bool time_inner = false);
+	void hash(uint8_t* ret, uint8_t const* header, uint64_t nonce, unsigned count);
+	void search(uint8_t const* header, uint64_t target, search_hook& hook);
 
 private:
+	static unsigned const c_max_search_results = 63;
 	static unsigned const c_num_buffers = 2;
-	static unsigned const c_batch_size = 1024*1024;
+	static unsigned const c_hash_batch_size = 1024;
+	static unsigned const c_search_batch_size = 1024*256;
 
 	ethash_params m_params;
 	cl::Context m_context;
 	cl::CommandQueue m_queue;
-	cl::Kernel m_kernel;
+	cl::Kernel m_hash_kernel;
+	cl::Kernel m_search_kernel;
 	cl::Buffer m_dag;
 	cl::Buffer m_header;
-	cl::Buffer m_hashes[c_num_buffers];
+	cl::Buffer m_hash_buf[c_num_buffers];
+	cl::Buffer m_search_buf[c_num_buffers];
+	unsigned m_workgroup_size;
 };
