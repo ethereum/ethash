e6ce1a9a2ae9fa8b97cf4751be631d198725ec48
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2015-03-09 03:13:25 -0400

    version bump

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup (
        author = "Matthew Wampler-Doty",
        author_email = "matthew.wampler.doty@gmail.com",
        license = 'GPL',
-       version = '20.2',
+       version = '20.3',
        url = 'https://github.com/ethereum/ethash',
        download_url = 'https://github.com/ethereum/ethash/tarball/v20.2',
        description = 'Python wrappers for ethash, the ethereum proof of work hashing function',
