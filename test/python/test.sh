3e28a57115856bced96a54f431cb94c68ddb6176
Lefteris Karapetsas <lefteris@refu.co>
2015-04-01 19:41:55 +0200

    Small changes to python/core.c after refactoring
    - Also added some special cases in python/test.sh for ArchLinux and made
      sure that the nose package of the virtualenv is used even if the
      system already has one

--- test/python/test.sh
+++ test/python/test.sh
@@ -3,6 +3,13 @@
 # Strict mode
 set -e
 
+# just a specific case for archlinux where python3 is the default
+if [ -f "/etc/arch-release" ]; then
+    VIRTUALENV_EXEC=virtualenv2
+else
+    VIRTUALENV_EXEC=virtualenv
+fi
+
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do
   DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
@@ -11,9 +18,11 @@ while [ -h "$SOURCE" ]; do
 done
 TEST_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
-[ -d $TEST_DIR/python-virtual-env ] || virtualenv --system-site-packages $TEST_DIR/python-virtual-env
+[ -d $TEST_DIR/python-virtual-env ] || $VIRTUALENV_EXEC --system-site-packages $TEST_DIR/python-virtual-env
 source $TEST_DIR/python-virtual-env/bin/activate
 pip install -r $TEST_DIR/requirements.txt > /dev/null
+# force installation of nose in virtualenv even if existing in thereuser's system
+pip install nose -I
 pip install --upgrade --no-deps --force-reinstall -e $TEST_DIR/../..
 cd $TEST_DIR
 nosetests --with-doctest -v --nocapture
