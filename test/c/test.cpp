a219ade9203ce8afbe814f61a5003ee0ada42f9e
Lefteris Karapetsas <lefteris@refu.co>
2015-04-20 12:54:16 +0200

    Some minor changes to the C API towards finalization

--- test/c/test.cpp
+++ test/c/test.cpp
@@ -235,7 +235,8 @@ static std::vector<char> readFileIntoVector(char const* filename)
 }
 
 BOOST_AUTO_TEST_CASE(light_and_full_client_checks) {
-	ethash_params params;
+	uint64_t full_size;
+	uint64_t cache_size;
 	ethash_h256_t seed;
 	ethash_h256_t hash;
 	ethash_h256_t difficulty;
@@ -250,17 +251,16 @@ BOOST_AUTO_TEST_CASE(light_and_full_client_checks) {
 	for (int i = 2; i < 32; i++)
 		ethash_h256_set(&difficulty, i, 255);
 
-	ethash_params_init(&params, 0);
-	params.cache_size = 1024;
-	params.full_size = 1024 * 32;
+	cache_size = 1024;
+	full_size = 1024 * 32;
 
-	ethash_light_t light = ethash_light_new(&params, &seed);
+	ethash_light_t light = ethash_light_new(cache_size, &seed);
 	ethash_cache *cache = ethash_light_get_cache(light);
 	BOOST_ASSERT(light);
 	ethash_full_t full = ethash_full_new(
 		"./test_ethash_directory/",
 		&seed,
-		&params,
+		full_size,
 		ethash_light_get_cache(light),
 		NULL
 	);
@@ -268,7 +268,7 @@ BOOST_AUTO_TEST_CASE(light_and_full_client_checks) {
 	{
 		const std::string
 				expected = "2da2b506f21070e1143d908e867962486d6b0a02e31d468fd5e3a7143aafa76a14201f63374314e2a6aaf84ad2eb57105dea3378378965a1b3873453bb2b78f9a8620b2ebeca41fbc773bb837b5e724d6eb2de570d99858df0d7d97067fb8103b21757873b735097b35d3bea8fd1c359a9e8a63c1540c76c9784cf8d975e995ca8620b2ebeca41fbc773bb837b5e724d6eb2de570d99858df0d7d97067fb8103b21757873b735097b35d3bea8fd1c359a9e8a63c1540c76c9784cf8d975e995ca8620b2ebeca41fbc773bb837b5e724d6eb2de570d99858df0d7d97067fb8103b21757873b735097b35d3bea8fd1c359a9e8a63c1540c76c9784cf8d975e995c259440b89fa3481c2c33171477c305c8e1e421f8d8f6d59585449d0034f3e421808d8da6bbd0b6378f567647cc6c4ba6c434592b198ad444e7284905b7c6adaf70bf43ec2daa7bd5e8951aa609ab472c124cf9eba3d38cff5091dc3f58409edcc386c743c3bd66f92408796ee1e82dd149eaefbf52b00ce33014a6eb3e50625413b072a58bc01da28262f42cbe4f87d4abc2bf287d15618405a1fe4e386fcdafbb171064bd99901d8f81dd6789396ce5e364ac944bbbd75a7827291c70b42d26385910cd53ca535ab29433dd5c5714d26e0dce95514c5ef866329c12e958097e84462197c2b32087849dab33e88b11da61d52f9dbc0b92cc61f742c07dbbf751c49d7678624ee60dfbe62e5e8c47a03d8247643f3d16ad8c8e663953bcda1f59d7e2d4a9bf0768e789432212621967a8f41121ad1df6ae1fa78782530695414c6213942865b2730375019105cae91a4c17a558d4b63059661d9f108362143107babe0b848de412e4da59168cce82bfbff3c99e022dd6ac1e559db991f2e3f7bb910cefd173e65ed00a8d5d416534e2c8416ff23977dbf3eb7180b75c71580d08ce95efeb9b0afe904ea12285a392aff0c8561ff79fca67f694a62b9e52377485c57cc3598d84cac0a9d27960de0cc31ff9bbfe455acaa62c8aa5d2cce96f345da9afe843d258a99c4eaf3650fc62efd81c7b81cd0d534d2d71eeda7a6e315d540b4473c80f8730037dc2ae3e47b986240cfc65ccc565f0d8cde0bc68a57e39a271dda57440b3598bee19f799611d25731a96b5dbbbefdff6f4f656161462633030d62560ea4e9c161cf78fc96a2ca5aaa32453a6c5dea206f766244e8c9d9a8dc61185ce37f1fc804459c5f07434f8ecb34141b8dcae7eae704c950b55556c5f40140c3714b45eddb02637513268778cbf937a33e4e33183685f9deb31ef54e90161e76d969587dd782eaa94e289420e7c2ee908517f5893a26fdb5873d68f92d118d4bcf98d7a4916794d6ab290045e30f9ea00ca547c584b8482b0331ba1539a0f2714fddc3a0b06b0cfbb6a607b8339c39bcfd6640b1f653e9d70ef6c985b",
-				actual = bytesToHexString((uint8_t const *) cache->mem, params.cache_size);
+				actual = bytesToHexString((uint8_t const *) cache->mem, cache_size);
 
 		BOOST_REQUIRE_MESSAGE(expected == actual,
 				"\nexpected: " << expected.c_str() << "\n"
@@ -276,7 +276,7 @@ BOOST_AUTO_TEST_CASE(light_and_full_client_checks) {
 	}
 	{
 		node node;
-		ethash_calculate_dag_item(&node, 0, &params, cache);
+		ethash_calculate_dag_item(&node, 0, cache);
 		const std::string
 				actual = bytesToHexString((uint8_t const *) &node, sizeof(node)),
 				expected = "b1698f829f90b35455804e5185d78f549fcb1bdce2bee006d4d7e68eb154b596be1427769eb1c3c3e93180c760af75f81d1023da6a0ffbe321c153a7c0103597";
@@ -285,10 +285,10 @@ BOOST_AUTO_TEST_CASE(light_and_full_client_checks) {
 						<< "actual: " << actual.c_str() << "\n");
 	}
 	{
-		for (int i = 0; i < params.full_size / sizeof(node); ++i) {
+		for (int i = 0; i < full_size / sizeof(node); ++i) {
 			for (uint32_t j = 0; j < 32; ++j) {
 				node expected_node;
-				ethash_calculate_dag_item(&expected_node, j, &params, cache);
+				ethash_calculate_dag_item(&expected_node, j, cache);
 				const std::string
 						actual = bytesToHexString((uint8_t const *) &(full->data[j]), sizeof(node)),
 						expected = bytesToHexString((uint8_t const *) &expected_node, sizeof(node));
@@ -301,8 +301,8 @@ BOOST_AUTO_TEST_CASE(light_and_full_client_checks) {
 	}
 	{
 		uint64_t nonce = 0x7c7c597c;
-		BOOST_REQUIRE(ethash_full_compute(&full_out, full, &params, &hash, nonce));
-		BOOST_REQUIRE(ethash_light_compute(&light_out, light, &params, &hash, nonce));
+		BOOST_REQUIRE(ethash_full_compute(&full_out, full, &hash, nonce));
+		BOOST_REQUIRE(ethash_light_compute(&light_out, light, full_size, &hash, nonce));
 		const std::string
 				light_result_string = blockhashToHexString(&light_out.result),
 				full_result_string = blockhashToHexString(&full_out.result);
@@ -323,14 +323,14 @@ BOOST_AUTO_TEST_CASE(light_and_full_client_checks) {
 						<< "full result: " << full_result_string.c_str() << "\n");
 	}
 	{
-		BOOST_REQUIRE(ethash_full_compute(&full_out, full, &params, &hash, 5));
+		BOOST_REQUIRE(ethash_full_compute(&full_out, full, &hash, 5));
 		std::string
 				light_result_string = blockhashToHexString(&light_out.result),
 				full_result_string = blockhashToHexString(&full_out.result);
 		BOOST_REQUIRE_MESSAGE(light_result_string != full_result_string,
 				"\nlight result and full result should differ: " << light_result_string.c_str() << "\n");
 
-		BOOST_REQUIRE(ethash_light_compute(&light_out, light, &params, &hash, 5));
+		BOOST_REQUIRE(ethash_light_compute(&light_out, light, full_size, &hash, 5));
 		light_result_string = blockhashToHexString(&light_out.result);
 		BOOST_REQUIRE_MESSAGE(light_result_string == full_result_string,
 				"\nlight result and full result should be the same\n"
@@ -373,27 +373,27 @@ static int test_full_callback_that_fails(unsigned _progress)
 }
 
 BOOST_AUTO_TEST_CASE(full_client_callback) {
-	ethash_params params;
+	uint64_t full_size;
+	uint64_t cache_size;
 	ethash_h256_t seed;
 	ethash_h256_t hash;
 	ethash_return_value full_out;
 	memcpy(&seed, "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~", 32);
 	memcpy(&hash, "~~~X~~~~~~~~~~~~~~~~~~~~~~~~~~~~", 32);
 
-	ethash_params_init(&params, 0);
-	params.cache_size = 1024;
-	params.full_size = 1024 * 32;
+	cache_size = 1024;
+	full_size = 1024 * 32;
 
-	ethash_cache *cache = ethash_cache_new(&params, &seed);
+	ethash_cache *cache = ethash_cache_new(cache_size, &seed);
 	ethash_full_t full = ethash_full_new(
 		"./test_ethash_directory/",
 		&seed,
-		&params,
+		full_size,
 		cache,
 		test_full_callback
 	);
 	BOOST_ASSERT(full);
-	BOOST_REQUIRE(ethash_full_compute(&full_out, full, &params, &hash, 5));
+	BOOST_REQUIRE(ethash_full_compute(&full_out, full, &hash, 5));
 	BOOST_CHECK(g_executed);
 	BOOST_REQUIRE_EQUAL(g_prev_progress, 100);
 
@@ -402,27 +402,27 @@ BOOST_AUTO_TEST_CASE(full_client_callback) {
 }
 
 BOOST_AUTO_TEST_CASE(failing_full_client_callback) {
-	ethash_params params;
+	uint64_t full_size;
+	uint64_t cache_size;
 	ethash_h256_t seed;
 	ethash_h256_t hash;
 	ethash_return_value full_out;
 	memcpy(&seed, "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~", 32);
 	memcpy(&hash, "~~~X~~~~~~~~~~~~~~~~~~~~~~~~~~~~", 32);
 
-	ethash_params_init(&params, 0);
-	params.cache_size = 1024;
-	params.full_size = 1024 * 32;
+	cache_size = 1024;
+	full_size = 1024 * 32;
 
-	ethash_cache *cache = ethash_cache_new(&params, &seed);
+	ethash_cache *cache = ethash_cache_new(cache_size, &seed);
 	ethash_full_t full = ethash_full_new(
 		"./test_ethash_directory/",
 		&seed,
-		&params,
+		full_size,
 		cache,
 		test_full_callback_that_fails
 	);
 	BOOST_ASSERT(full);
-	BOOST_REQUIRE(!ethash_full_compute(&full_out, full, &params, &hash, 5));
+	BOOST_REQUIRE(!ethash_full_compute(&full_out, full, &hash, 5));
 	ethash_full_delete(full);
 	fs::remove_all("./test_ethash_directory/");
 }
@@ -457,9 +457,10 @@ BOOST_AUTO_TEST_CASE(light_and_full_client_checks_old) {
 	params.full_size = 1024 * 32;
 	ethash_cache cache;
 	cache.mem = our_alloca(params.cache_size);
+    cache.cache_size = params.cache_size;
 	ethash_mkcache(&cache, &params, &seed);
 	node *full_mem = (node *) our_alloca(params.full_size);
-	ethash_compute_full_data(full_mem, &params, &cache);
+	ethash_compute_full_data(full_mem, params.full_size, &cache);
 
 	{
 		const std::string
@@ -474,7 +475,7 @@ BOOST_AUTO_TEST_CASE(light_and_full_client_checks_old) {
 
 	{
 		node node;
-		ethash_calculate_dag_item(&node, 0, &params, &cache);
+		ethash_calculate_dag_item(&node, 0, &cache);
 		const std::string
 				actual = bytesToHexString((uint8_t const *) &node, sizeof(node)),
 				expected = "b1698f829f90b35455804e5185d78f549fcb1bdce2bee006d4d7e68eb154b596be1427769eb1c3c3e93180c760af75f81d1023da6a0ffbe321c153a7c0103597";
@@ -487,7 +488,7 @@ BOOST_AUTO_TEST_CASE(light_and_full_client_checks_old) {
 		for (int i = 0; i < params.full_size / sizeof(node); ++i) {
 			for (uint32_t j = 0; j < 32; ++j) {
 				node expected_node;
-				ethash_calculate_dag_item(&expected_node, j, &params, &cache);
+				ethash_calculate_dag_item(&expected_node, j, &cache);
 				const std::string
 						actual = bytesToHexString((uint8_t const *) &(full_mem[j]), sizeof(node)),
 						expected = bytesToHexString((uint8_t const *) &expected_node, sizeof(node));
