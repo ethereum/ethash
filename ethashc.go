119bd48aaba32b659b871780f87efa3e9a375027
Gustav Simonsson <gustav.simonsson@gmail.com>
2015-05-07 01:17:27 +0200

    Add comment about stack probing and remove unused ifdefs

--- ethashc.go
+++ ethashc.go
@@ -1,6 +1,17 @@
 package ethash
 
 /*
+ -mno-stack-arg-probe disables stack probing which avoids the function
+ __chkstk_ms being linked. this avoids a clash of this symbol as we also
+ separately link the secp256k1 lib which ends up defining this symbol
+
+ 1. https://gcc.gnu.org/onlinedocs/gccint/Stack-Checking.html
+ 2. https://groups.google.com/forum/#!msg/golang-dev/v1bziURSQ4k/88fXuJ24e-gJ
+ 3. https://groups.google.com/forum/#!topic/golang-nuts/VNP6Mwz_B6o
+
+*/
+
+/*
 #cgo CFLAGS: -std=gnu99 -Wall
 #cgo windows CFLAGS: -mno-stack-arg-probe
 #cgo LDFLAGS: -lm
